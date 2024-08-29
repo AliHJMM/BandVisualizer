68a1ae347a6eb5a0dc963d1f158bd28bd7c65abf
Ali <AliHasanJMM@gmail.com>
2024-08-29 04:30:34 +0300

    Main

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"groupie-tracker/server"
+)
+
+func main() {
+	http.HandleFunc("/", server.HomeHandler)
+	http.HandleFunc("/details", server.DetailsHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	fmt.Println("Server running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":2004", nil))
+}
