b62ce51a818ece082321c12ccee7c6c7ffbc8f8a
Ali <AliHasanJMM@gmail.com>
2024-11-14 09:55:18 +0300

    Inshallah no more errors

--- API/ArtistAPI.go
+++ API/ArtistAPI.go
@@ -1,70 +1,77 @@
+// ArtistAPI.go
 package API
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"sync"
 )
 
 var (
-	cachedArtists   []Artist
-	cachedLocations map[int]LocationData
-	artistOnce      sync.Once
-	locationOnce    sync.Once
+    cachedArtists   []Artist
+    cachedLocations map[int][]string
+    artistOnce      sync.Once
+    locationOnce    sync.Once
 )
 
 func FetchArtists() ([]Artist, error) {
-	var err error
-	artistOnce.Do(func() {
-		resp, e := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-		if e != nil {
-			err = e
-			return
-		}
-		defer resp.Body.Close()
+    var err error
+    artistOnce.Do(func() {
+        resp, e := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+        if e != nil {
+            err = e
+            return
+        }
+        defer resp.Body.Close()
 
-		var artists []Artist
-		if e := json.NewDecoder(resp.Body).Decode(&artists); e != nil {
-			err = e
-			return
-		}
-		cachedArtists = artists
-	})
-	return cachedArtists, err
+        var artists []Artist
+        if e := json.NewDecoder(resp.Body).Decode(&artists); e != nil {
+            err = e
+            return
+        }
+        cachedArtists = artists
+    })
+    return cachedArtists, err
 }
 
-func FetchAllLocations() (map[int]LocationData, error) {
-	var err error
-	locationOnce.Do(func() {
-		resp, e := http.Get("https://groupietrackers.herokuapp.com/api/locations")
-		if e != nil {
-			err = e
-			return
-		}
-		defer resp.Body.Close()
+func FetchAllLocations() (map[int][]string, error) {
+    var err error
+    locationOnce.Do(func() {
+        resp, e := http.Get("https://groupietrackers.herokuapp.com/api/locations")
+        if e != nil {
+            err = e
+            return
+        }
+        defer resp.Body.Close()
 
-		var allLocations []struct {
-			ID        int         `json:"id"`
-			Locations LocationData `json:"locations"`
-		}
+        var data AllLocationsData
+        if e := json.NewDecoder(resp.Body).Decode(&data); e != nil {
+            err = e
+            return
+        }
 
-		if e := json.NewDecoder(resp.Body).Decode(&allLocations); e != nil {
-			err = e
-			return
-		}
+        cachedLocations = make(map[int][]string)
+        for _, locEntry := range data.Index {
+            cachedLocations[locEntry.ID] = locEntry.Locations
+        }
 
-		cachedLocations = make(map[int]LocationData)
-		for _, loc := range allLocations {
-			cachedLocations[loc.ID] = loc.Locations
-		}
-	})
-	return cachedLocations, err
+        if len(cachedLocations) == 0 {
+            log.Println("No locations data found.")
+            err = fmt.Errorf("no locations data found")
+            return
+        }
+    })
+
+    return cachedLocations, err
 }
+
 
 func GetArtistsLength() (int, error) {
-	artists, err := FetchArtists()
-	if err != nil {
-		return 0, err
-	}
-	return len(artists), nil
+    artists, err := FetchArtists()
+    if err != nil {
+        return 0, err
+    }
+    return len(artists), nil
 }
--- API/Struct.go
+++ API/Struct.go
@@ -1,4 +1,9 @@
+// Struct.go
 package API
+
+type LocationData struct {
+    Locations []string `json:"locations"`
+}
 
 type Artist struct {
     ID           int      `json:"id"`
@@ -12,29 +17,37 @@ type Artist struct {
     Relations    string   `json:"relations"`
 }
 
-type LocationData struct {
-	Locations []string `json:"locations"`
+type LocationEntry struct {
+    ID        int      `json:"id"`
+    Locations []string `json:"locations"`
+    Dates     string   `json:"dates"`
 }
 
+type AllLocationsData struct {
+    Index []LocationEntry `json:"index"`
+}
+
+// Existing structs remain unchanged
 type DatesData struct {
-	Dates []string `json:"dates"`
+    Dates []string `json:"dates"`
 }
 
 type RelationData struct {
-	DatesLocation map[string][]string `json:"datesLocations"`
+    DatesLocation map[string][]string `json:"datesLocations"`
 }
+
 type Details struct {
-	ID           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
+    ID           int      `json:"id"`
+    Image        string   `json:"image"`
+    Name         string   `json:"name"`
+    Members      []string `json:"members"`
+    CreationDate int      `json:"creationDate"`
+    FirstAlbum   string   `json:"firstAlbum"`
 }
 
 type AllDetails struct {
-	Details   Details
-	Dates     DatesData
-	Relations RelationData
-	Location  LocationData
+    Details   Details
+    Dates     DatesData
+    Relations RelationData
+    Location  LocationData
 }
