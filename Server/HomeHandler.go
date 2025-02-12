a981014a2268f99aeb107d431451ae305aa7f2a0
Ali <AliHasanJMM@gmail.com>
2025-02-12 03:23:50 +0300

    Working

--- Server/HomeHandler.go
+++ Server/HomeHandler.go
@@ -2,37 +2,47 @@ package server
 
 import (
 	"BandVisualizer/API"
+	"html/template"
 	"net/http"
-	"text/template"
 )
 
+// ErrorData represents the error message and status code passed to the error template.
 type ErrorData struct {
-	ErrorMessage string
-	Code         string
+    ErrorMessage string
+    Code         string
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		renderErrorPage(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	if r.URL.Path != "/" {
-		renderErrorPage(w, "Not Found", http.StatusNotFound)
-		return
-	}
-	artists, err := API.FetchArtists()
-	if err != nil {
-		renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	tmpl, err := template.ParseFiles("static/html/index.html")
-	if err != nil {
-		renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	err = tmpl.Execute(w, artists)
-	if err != nil {
-		renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+    if r.Method != "GET" {
+        renderErrorPage(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        return
+    }
+    if r.URL.Path != "/" {
+        renderErrorPage(w, "Not Found", http.StatusNotFound)
+        return
+    }
+    artists, err := API.FetchArtists()
+    if err != nil {
+        renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    locations, err := AggregateLocations()
+    if err != nil {
+        renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    data := PageData{
+        Artists:   artists,
+        Locations: locations,
+    }
+    tmpl, err := template.ParseFiles("static/html/index.html")
+    if err != nil {
+        renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    err = tmpl.Execute(w, data)
+    if err != nil {
+        renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
 }
