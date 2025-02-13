f362761b06dc87550379c48fe21dd61587fee6f0
Ali <AliHasanJMM@gmail.com>
2025-02-13 19:08:16 +0300

    Added Geolocation feature

--- Server/DetailsHandler.go
+++ Server/DetailsHandler.go
@@ -1,10 +1,19 @@
+// server/DetailsHandler.go
 package server
 
 import (
 	"BandVisualizer/API"
+	"encoding/json"
 	"html/template"
 	"net/http"
 )
+
+// DetailsTemplateData embeds the AllDetails data and adds pre‑marshaled JSON strings.
+type DetailsTemplateData struct {
+	API.AllDetails
+	LocJSON template.JS // JSON representation of Location.Locations
+	RelJSON template.JS // JSON representation of Relations.DatesLocation
+}
 
 func DetailsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
@@ -14,7 +23,6 @@ func DetailsHandler(w http.ResponseWriter, r *http.Request) {
 
 	id := r.URL.Query().Get("id")
 	isValid := CheckId(id)
-
 	if !isValid {
 		renderErrorPage(w, "Not Found", http.StatusNotFound)
 		return
@@ -26,12 +34,32 @@ func DetailsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Marshal location and relation data into JSON strings.
+	locBytes, err := json.Marshal(data.Location.Locations)
+	if err != nil {
+		renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	relBytes, err := json.Marshal(data.Relations.DatesLocation)
+	if err != nil {
+		renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Prepare the template data.
+	tplData := DetailsTemplateData{
+		AllDetails: data,
+		LocJSON:    template.JS(locBytes), // safe to output as JS
+		RelJSON:    template.JS(relBytes),
+	}
+
 	tmpl, err := template.ParseFiles("static/html/details.html")
 	if err != nil {
 		renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	err = tmpl.Execute(w, data)
+
+	err = tmpl.Execute(w, tplData)
 	if err != nil {
 		renderErrorPage(w, "Internal Server Error", http.StatusInternalServerError)
 		return
